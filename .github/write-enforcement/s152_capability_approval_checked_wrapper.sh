8f2c8a1f5b195bc568b35c3de61037dec66176c3
WEA durable history <wea-history@users.noreply.github.com>
2026-09-12 21:59:44 +0000

    Durable WEA run disposition 31920812835

--- .github/write-enforcement/s152_capability_approval_checked_wrapper.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-printf 'REFUSE(S152_CAPABILITY_APPROVAL_WRAPPER): WITHDRAWN_PUBLIC_LATEST_REPAIR_REQUIRED\n' >&2
-printf 'SAFE_TO_PASTE_BACK=true secret_bytes_printed=false\n' >&2
-exit 2
