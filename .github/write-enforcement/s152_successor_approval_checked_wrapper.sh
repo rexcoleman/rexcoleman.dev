3a7b8bb07f15886c594c5a3e51c5ab7164417b54
WEA durable history <wea-history@users.noreply.github.com>
2026-09-20 11:31:08 +0000

    Durable WEA run disposition 35507952020

--- .github/write-enforcement/s152_successor_approval_checked_wrapper.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-printf 'REFUSE(S152_SUCCESSOR_APPROVAL_WRAPPER): WITHDRAWN_DYNAMIC_PREDECESSOR_REQUIRED\n' >&2
-printf 'SAFE_TO_PASTE_BACK=true owner_credential_handling=false\n' >&2
-exit 2
